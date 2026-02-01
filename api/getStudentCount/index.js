bc4892f0c848d38c1e7d8c676d929a17d07257c8
VENKATA LAKSHMI SAHITHI POLURI <polurisahithi2@gmail.com>
2026-02-01 18:17:16 -0500

    Update index.js

--- api/getStudentCount/index.js
+++ api/getStudentCount/index.js
@@ -1,1 +1,12 @@
-
+module.exports = async function (context, req) {
+    context.res = {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: [
+            { country: "USA", count: 120 },
+            { country: "India", count: 85 }
+        ]
+    };
+};
